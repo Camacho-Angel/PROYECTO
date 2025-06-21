064abd18a8da2909f5007afbabee8b1584b1998f
Camacho-Angel <angelemmanuelcamacho0@gmail.com>
2025-06-21 13:51:29 -0600

    Update proyecto1.py
    Se le agrega una ventana secundaria

--- proyecto1.py
+++ proyecto1.py
@@ -1,17 +1,44 @@
 import tkinter as tk
 from tkinter import ttk
-#codigo de color:
+#====================================================================================codigo de color:======================================================================================================================0
+azul_principal = "#004d66"
+azul_claro = "#e6f2f5"
 blanco = "#ffffff"
+gris = "#f2f2f2"
 azul_principal = "#1e3d59"
 azul_secundario = "#a8d0e6"
 azul_oscuro = "#162c42"
+fuente_general = ("Segoe UI", 11)
+fuente_titulo = ("Segoe UI", 16, "bold")
 fuente_boton = ("Segoe UI", 11, "bold")
+#==============================================Codigo de la ventana secundaria de Eleccion=================================================================0
+def abrirseleccion():#funcion que permite abrir esta ventana
+    ventanadeeleccion = tk.Toplevel(ventanaprincipal)
+    ventanadeeleccion.title("Seleccionar rol")
+    ventanadeeleccion.geometry("420x320")
+    ventanadeeleccion.configure(bg=azul_secundario)
+    #Encabezado
+    encabezado = tk.Frame(ventanadeeleccion, bg=azul_principal, height=60)
+    encabezado.pack(fill="x")
+    tk.Label(encabezado, text="¿Quién eres?", fg=blanco, bg=azul_principal,font=fuente_titulo, pady=10).pack()
+
+    menu = tk.Frame(ventanadeeleccion, bg=azul_secundario, pady=20)
+    menu.pack(expand=True)
+
+    tk.Button(menu, text="Paciente", font=fuente_boton, bg=azul_principal, fg=blanco,activebackground=azul_oscuro, activeforeground=blanco,
+                  bd=0, relief="ridge", padx=20, pady=8).pack(pady=10, fill="x")
+    tk.Button(menu, text="Doctor", font=fuente_boton, bg=azul_principal, fg=blanco,activebackground=azul_oscuro, activeforeground=blanco,
+                  bd=0, relief="ridge", padx=20, pady=8).pack(pady=10, fill="x")
+    tk.Button(menu, text="Enfermero", font=fuente_boton, bg=azul_principal, fg=blanco,activebackground=azul_oscuro, activeforeground=blanco,
+                  bd=0, relief="ridge", padx=20, pady=8).pack(pady=10, fill="x")
+    
+#==============================================================================================Ventana Principal===========================================================================================================
 ventanaprincipal = tk.Tk()
 ventanaprincipal.title("Hospital Ángeles")
 ventanaprincipal.geometry("450x350")
 ventanaprincipal.configure(bg=azul_secundario)
 tk.Label(ventanaprincipal, text="HOSPITAL ÁNGELES", font=("Segoe UI", 22, "bold"),fg=azul_principal, bg=azul_secundario ).pack(pady=20)
 tk.Label(ventanaprincipal, text="Donde la salud y el cuidado se encuentran", font=("Segoe UI", 12, "italic"), fg=azul_oscuro, bg=azul_secundario).pack(pady=(0, 30))
-tk.Button(ventanaprincipal, text="Ingresar", font=fuente_boton, bg=azul_principal, fg=blanco, activebackground=azul_oscuro, activeforeground=blanco,bd=0, relief="flat", width=18, pady=10).pack(pady=8)
+tk.Button(ventanaprincipal, text="Ingresar", font=fuente_boton, bg=azul_principal, fg=blanco, activebackground=azul_oscuro, activeforeground=blanco,bd=0, relief="flat", width=18, pady=10, command=abrirseleccion).pack(pady=8)
 tk.Button(ventanaprincipal, text="Salir", font=fuente_boton, bg=azul_principal, fg=blanco, activebackground=azul_oscuro, activeforeground=blanco,bd=0, relief="flat", width=18, pady=10,command=ventanaprincipal.destroy ).pack(pady=8)
 ventanaprincipal.mainloop()
