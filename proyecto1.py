a299d8705f7bb4d7e4319db298807f2aeb25b01f
Camacho-Angel <angelemmanuelcamacho0@gmail.com>
2025-06-22 00:25:05 -0600

    Update proyecto1.py
    Se agrega una ventana que nos permite registra médicos y administrar el personal disponible

--- proyecto1.py
+++ proyecto1.py
@@ -174,6 +174,139 @@ def mostrar_pacientes():
 
     tree.pack(expand=True, fill=tk.BOTH)
 
+def guardar_doctor(entry_nombre, entry_apellidopaterno, entry_apellidomaterno, combo_genero, entry_edad, combo_area, combo_especialidad):
+    nombre = entry_nombre.get().strip()
+    apellidopa = entry_apellidopaterno.get().strip()
+    apellidoma = entry_apellidomaterno.get().strip()
+    genero = combo_genero.get().strip()
+    edad = entry_edad.get().strip()
+    area = combo_area.get().strip()
+    especialidad = combo_especialidad.get().strip()
+
+    if not nombre or not apellidopa or not apellidoma or not genero or not edad or not area or not especialidad:
+        messagebox.showerror("Error", "Complete todos los campos")
+        return
+
+    doctores.append(Doctor(nombre, apellidopa, apellidoma, genero, edad, area, especialidad))
+    messagebox.showinfo("Éxito", f"Doctor {nombre} registrado.")
+    
+    entry_nombre.delete(0, tk.END)
+    entry_apellidopaterno.delete(0, tk.END)
+    entry_apellidomaterno.delete(0, tk.END)
+    combo_genero.delete(0, tk.END)
+    entry_edad.delete(0, tk.END)
+    combo_area.delete(0, tk.END)
+    combo_especialidad.delete(0, tk.END)
+
+def mostrar_personal():
+    ventana = tk.Toplevel()
+    ventana.title("Personal")
+
+    cols = ("Nombre", "Apellido Paterno", "Apellido Materno", "Genero", "Edad", "Área", "Tipo", "Especialidad", "Ocupado")
+    tree = ttk.Treeview(ventana, columns=cols, show="headings")
+
+    for col in cols:
+        tree.heading(col, text=col)
+        tree.column(col, width=120)
+
+    for d in doctores:
+        tree.insert("", tk.END, values=(d.nombre, d.apellidopa, d.apellidoma, d.genero, d.edad, d.area, "Doctor", d.especialidad, "Sí" if d.ocupado else "No"))
+    for e in enfermeros:  
+        tree.insert("", tk.END, values=(e.nombre, e.apellidopa, e.apellidoma, e.genero, e.edad, e.area, "Enfermero", e.especialidad, "Sí" if e.ocupado else "No"))
+
+    tree.pack(expand=True, fill=tk.BOTH)
+
+    def editar_personal():
+        try:
+            item = tree.selection()[0]
+            nombre = tree.item(item, 'values')[0]
+            apellidopaterno = tree.item(item, 'values')[1]
+            apellidomaterno = tree.item(item, 'values')[2]
+            tipo = tree.item(item, 'values')[6]
+
+            personal = next((p for p in (doctores if tipo == "Doctor" else enfermeros) if p.nombre == nombre and p.apellidopa == apellidopaterno and p.apellidoma == apellidomaterno), None)
+
+            ventana_editar = tk.Toplevel()
+            ventana_editar.title("Editar Personal")
+            ventana_editar.configure(bg=azul_secundario)
+            ventana_editar.lift()
+
+            tk.Label(ventana_editar, text="Nombre:",bg=azul_secundario).grid(row=0, column=0)
+            entry_nombre_ed = tk.Entry(ventana_editar)
+            entry_nombre_ed.insert(0, personal.nombre)
+            entry_nombre_ed.grid(row=0, column=1)
+
+            tk.Label(ventana_editar, text="Apellido Paterno:",bg=azul_secundario).grid(row=1, column=0)
+            entry_apellidopaterno_ed = tk.Entry(ventana_editar)
+            entry_apellidopaterno_ed.insert(0, personal.apellidopa)
+            entry_apellidopaterno_ed.grid(row=1, column=1)
+
+            tk.Label(ventana_editar, text="Apellido Materno:",bg=azul_secundario).grid(row=3, column=0)
+            entry_apellidomaterno_ed = tk.Entry(ventana_editar)
+            entry_apellidomaterno_ed.insert(0, personal.apellidoma)
+            entry_apellidomaterno_ed.grid(row=3, column=1)
+
+            tk.Label(ventana_editar, text="Genero:",bg=azul_secundario).grid(row=4, column=0)
+            entry_genero_ed = ttk.Combobox(ventana_editar, values= ["Masculino", "Femenino"])
+            entry_genero_ed.set(personal.genero)
+            entry_genero_ed.grid(row=4, column=1)
+
+            tk.Label(ventana_editar, text="Edad:",bg=azul_secundario).grid(row=5, column=0)
+            entry_edad_ed = tk.Entry(ventana_editar)
+            entry_edad_ed.insert(0, personal.edad)
+            entry_edad_ed.grid(row=5, column=1)
+
+            tk.Label(ventana_editar, text="Área:",bg=azul_secundario).grid(row=6, column=0)
+            combo_area_ed = ttk.Combobox(ventana_editar, values=list(camillas.keys()), state="readonly")
+            combo_area_ed.set(personal.area)
+            combo_area_ed.grid(row=6, column=1)
+
+            tk.Label(ventana_editar, text="Especialidad:",bg=azul_secundario).grid(row=7, column=0)
+            entry_especialidad_ed = ttk.Combobox(ventana_editar, values= ["Medicina Interna", "Cardiologia", "Pediatria", "Ginecologia", "Ninguna"])
+            entry_especialidad_ed.set(personal.especialidad)
+            entry_especialidad_ed.grid(row=7, column=1)
+
+            def guardar_edicion():
+                personal.nombre = entry_nombre_ed.get().strip()
+                personal.apellidopa = entry_apellidopaterno_ed.get().strip()
+                personal.apellidoma = entry_apellidomaterno_ed.get().strip()
+                personal.genero = entry_genero_ed.get().strip()
+                personal.edad = entry_edad_ed.get().strip()
+                personal.area = combo_area_ed.get().strip()
+                personal.especialidad = entry_especialidad_ed.get().strip()
+                messagebox.showinfo("Éxito", f"{tipo} actualizado correctamente.")
+                ventana_editar.destroy()
+                ventana.destroy()
+                mostrar_personal()
+
+            tk.Button(ventana_editar, text="Guardar",bg="#0cb804", command=guardar_edicion).grid(row=8, column=0, columnspan=8)
+
+        except IndexError:
+            messagebox.showwarning("Advertencia", "Seleccione un personal para editar.")
+
+    def eliminar_personal():
+        try:
+            item = tree.selection()[0]
+            nombre = tree.item(item, 'values')[0]
+            apellidopaterno = tree.item(item, 'values')[1]
+            apellidomaterno = tree.item(item, 'values')[2]
+            tipo = tree.item(item, 'values')[6]
+
+            if tipo == "Doctor":
+                doctores[:] = [d for d in doctores if d.nombre != nombre and d.apellidopa != apellidopaterno and d.apellidoma != apellidomaterno]
+            else:
+                enfermeros[:] = [e for e in enfermeros if e.nombre != nombre and e.apellidopa != apellidopaterno and e.apellidoma != apellidomaterno]
+
+            messagebox.showinfo("Éxito", f"{tipo} {nombre} {apellidopaterno} eliminado correctamente.")
+            ventana.destroy()
+            mostrar_personal()
+
+        except IndexError:
+            messagebox.showwarning("Advertencia", "Seleccione un personal para eliminar.")
+
+    tk.Button(ventana, text="Editar", bg="#0cb804",fg=blanco,command=editar_personal).pack(side=tk.LEFT, padx=10)
+    tk.Button(ventana, text="Eliminar", bg="#ef0808", fg=blanco, command=eliminar_personal).pack(side=tk.LEFT, padx=10)      
+
 #==============================================Codigo de la ventana secundaria de Eleccion=================================================================0
 def abrirseleccion():#funcion que permite abrir esta ventana
     ventanadeeleccion = tk.Toplevel(ventanaprincipal)
@@ -191,7 +324,7 @@ def abrirseleccion():#funcion que permite abrir esta ventana
     tk.Button(menu, text="Paciente", font=fuente_boton, bg=azul_principal, fg=blanco,activebackground=azul_oscuro, activeforeground=blanco,
                   bd=0, relief="ridge", padx=20, pady=8, command=lambda: registropacientes(ventanadeeleccion)).pack(pady=10, fill="x")
     tk.Button(menu, text="Doctor", font=fuente_boton, bg=azul_principal, fg=blanco,activebackground=azul_oscuro, activeforeground=blanco,
-                  bd=0, relief="ridge", padx=20, pady=8).pack(pady=10, fill="x")
+                  bd=0, relief="ridge", padx=20, pady=8, command=lambda: registrodoctores(ventanadeeleccion)).pack(pady=10, fill="x")
     tk.Button(menu, text="Enfermero", font=fuente_boton, bg=azul_principal, fg=blanco,activebackground=azul_oscuro, activeforeground=blanco,
                   bd=0, relief="ridge", padx=20, pady=8).pack(pady=10, fill="x")
     
@@ -275,7 +408,64 @@ def registropacientes(ventanadeeleccion):
     menupacientes.add_cascade(label="Opciones", menu=opciones)
     ventanapacientes.config(menu=menupacientes)
   
+#==================================================Codigo de la ventana de Doctores==========================================================
+def registrodoctores(ventanadeeleccion):
+    ventanadeeleccion.withdraw()
+    ventanadoctores=tk.Toplevel(ventanadeeleccion)
+    ventanadoctores.title("Registro de Doctores")
+    ventanadoctores.configure(bg=gris)
+    # Encabezado
+    encabezado = tk.Frame(ventanadoctores, bg=azul_principal, height=50)
+    encabezado.pack(side="top", fill="x")
+    tk.Label(encabezado, text="Formulario de Doctores", fg="white", bg=azul_principal,font=("Helvetica", 16, "bold")).pack(pady=10)
+    #Formulario de registro del doctor
+    frame_formulario2 = tk.Frame(ventanadoctores, bg=gris)
+    frame_formulario2.pack()
+
+    tk.Label(frame_formulario2, text="Nombres:", bg=gris, font=fuente_general).grid(row=0, column=0)
+    entry_nombre= tk.Entry(frame_formulario2, font=fuente_general)
+    entry_nombre.grid(row=0, column=1)
     
+    tk.Label(frame_formulario2, text="Apellido paterno:", bg=gris, font=fuente_general).grid(row=1, column=0)
+    entry_apellidopaterno= tk.Entry(frame_formulario2, font=fuente_general)
+    entry_apellidopaterno.grid(row=1, column=1)
+    
+    tk.Label(frame_formulario2, text="Apellido materno:", bg=gris, font=fuente_general).grid(row=2, column=0)
+    entry_apellidomaterno = tk.Entry(frame_formulario2, font=fuente_general)
+    entry_apellidomaterno.grid(row=2, column=1)
+
+    tk.Label(frame_formulario2, text="Genero:", bg=gris, font=fuente_general).grid(row=3, column=0)
+    combo_genero = ttk.Combobox(frame_formulario2, values=["Masculino", "Femenino"])
+    combo_genero.grid(row=3, column=1)
+    combo_genero.current(0)
+
+    tk.Label(frame_formulario2, text="Edad:", bg=gris, font=fuente_general).grid(row=4, column=0)
+    entry_edad = tk.Entry(frame_formulario2, font=fuente_general)
+    entry_edad.grid(row=4, column=1)
+
+    tk.Label(frame_formulario2, text="Área:", bg=gris, font=fuente_general).grid(row=5, column=0)
+    combo_area = ttk.Combobox(frame_formulario2, values=list(camillas.keys()), state="readonly")
+    combo_area.grid(row=5, column=1)
+
+    tk.Label(frame_formulario2, text="Especialidad:", bg=gris, font=fuente_general).grid(row=6, column=0)
+    combo_especialidad = ttk.Combobox(frame_formulario2, values=["Medicina Interna", "Cardiologia", "Pediatria", "Gineclogia", "Ninguna"])
+    combo_especialidad.grid(row=6, column=1)
+    combo_especialidad.current(0)
+
+    tk.Button(frame_formulario2, text="Registrar Doctor",font=fuente_boton, bg=azul_principal,fg=blanco, command=lambda: guardar_doctor(entry_nombre, entry_apellidopaterno, entry_apellidomaterno, combo_genero, entry_edad, combo_area, combo_especialidad)).grid(row=12, column=0, columnspan=2, pady=5)
+
+    def regresar():
+        ventanadoctores.destroy()
+        ventanadeeleccion.deiconify()
+
+    menudoctores=tk.Menu(ventanadoctores)
+    opciones= tk.Menu(menudoctores, tearoff=0)
+    opciones.add_command(label="Regresar", command=regresar)
+    opciones.add_command(label="Mostrar pacientes", command=mostrar_pacientes)
+    opciones.add_command(label="Administrar personal", command=mostrar_personal)
+    menudoctores.add_cascade(label="Opciones", menu=opciones)
+    ventanadoctores.config(menu=menudoctores)
+   
 #==============================================================================================Ventana Principal===========================================================================================================
 ventanaprincipal = tk.Tk()
 ventanaprincipal.title("Hospital Ángeles")
